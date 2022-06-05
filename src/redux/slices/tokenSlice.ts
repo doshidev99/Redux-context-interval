e1683fb98fbe11caaca28df80a278e6282cf300b
jordanhazashi <truongpd@alphatrue.com>
2022-06-05 13:21:33 +0700

    wapper error

--- src/redux/slices/tokenSlice.ts
+++ src/redux/slices/tokenSlice.ts
@@ -2,18 +2,29 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { response } from './mockData';
 
 export function fetchRequest() {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: response }), 0)
+  return new Promise((resolve, reject) =>
+    // successfully
+    // setTimeout(() => resolve({ data: response }), 0)
+
+    // failed
+    setTimeout(() => reject({ data: 'Something error' }), 0)
   );
 }
 const initialState = {
   data: [],
 };
 
-export const fetchDataUserList = createAsyncThunk('token/user', async () => {
-  const response = (await fetchRequest()) as { data: any[] };
-  return response.data;
-});
+export const fetchDataUserList = createAsyncThunk(
+  'token/user',
+  async (payload, { rejectWithValue }) => {
+    try {
+      const response = (await fetchRequest()) as { data: any[] };
+      return response.data;
+    } catch (err) {
+      return rejectWithValue(err.data);
+    }
+  }
+);
 
 export const tokenManagementSlice = createSlice({
   name: 'token-management',
@@ -23,11 +34,17 @@ export const tokenManagementSlice = createSlice({
       state.data = action.payload;
     },
   },
-  extraReducers: {
-    // @ts-ignore
-    [fetchDataUserList.fulfilled]: (state, action) => {
-      state.data = action.payload;
-    },
+  extraReducers: (builder) => {
+    builder.addCase(fetchDataUserList.pending, (state) => {
+      // pending status
+    });
+    builder.addCase(fetchDataUserList.fulfilled, (state, { payload }) => {
+      state.data = payload;
+    });
+
+    builder.addCase(fetchDataUserList.rejected, (state, { payload }) => {
+      console.log(payload, '%c<----');
+    });
   },
 });
 
